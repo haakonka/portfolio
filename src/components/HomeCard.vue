23d7a9a089cd75cda2828c024a8c7185efe8c321
haakonka <haakon.kanter@gmail.com>
2022-08-18 22:04:14 +0200

    Fix github link

--- src/components/HomeCard.vue
+++ src/components/HomeCard.vue
@@ -1,61 +1,58 @@
 <template>
   <div class="home-container">
     <h1>{{ msg }}</h1>
-    <p>
-      Dataingeniørstudent på NTNU
-    </p>
-    <p> 21 år gammel</p>
+    <p>Dataingeniørstudent på NTNU</p>
+    <p>21 år gammel</p>
     <div class="container">
-      <a href= "https:www.github.com/haakonka"> <img alt="Github" src="../assets/githublight32.png" /></a>
+      <a href="https://www.github.com/haakonka">
+        <img alt="Github" src="../assets/githublight32.png"
+      /></a>
       <a href="mailto:haakon.kanter@gmail.com">
-<span class="material-icons">
-mail
-</span>
-</a>
+        <span class="material-icons"> mail </span>
+      </a>
 
-     <a href = "https://discordapp.com/channels/@me/320173768724971520/"> <img class= "discord" alt="Discord" src="../assets/discordlight.png" /> </a>
-
-</div>
+      <a href="https://discordapp.com/channels/@me/320173768724971520/">
+        <img class="discord" alt="Discord" src="../assets/discordlight.png" />
+      </a>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomePage',
+  name: "HomePage",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+};
 </script>
 
-
 <style scoped>
-
-a{
+a {
   color: rgba(234, 240, 246, 0.8);
 }
-.home-container{
+.home-container {
   margin: auto 0;
   border-radius: 0.6em;
   padding: 2em;
-  border: 2px solid  #356D8E;
+  border: 2px solid #356d8e;
 }
-.material-icons { font-size: 32px; }
+.material-icons {
+  font-size: 32px;
+}
 h3 {
   margin: 40px 0 0;
 }
 
-img{
+img {
   padding-right: 1em;
   padding-left: 1em;
 }
 
-
-.discord{
+.discord {
   padding-left: 0.5em;
   object-fit: contain;
   width: 32px;
   height: 32px;
 }
-
 </style>
