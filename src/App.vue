f1bd1d945c844374ea6a7518ca8f18abff362455
haakonka <haakon.kanter@gmail.com>
2022-08-23 14:29:07 +0200

    Added github link for Python Playground project

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
   <nav>
-    <router-link to="/">HJEM</router-link> 
+    <router-link to="/">HJEM</router-link>
     <router-link to="/projects"> PROSJEKTER</router-link>
     <router-link to="/interests"> INTERESSER</router-link>
     <router-link to="/skills"> FERDIGHETER</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
-
-body{
+body {
   background-color: #121212;
 }
 
@@ -24,60 +23,55 @@ body{
 }
 
 nav {
-  padding: 30px; 
+  padding: 30px;
 }
 
 nav a {
-    margin-right: 30px;
-    text-decoration: none;
-    color: rgba(255, 255, 255, 0.6);
+  margin-right: 30px;
+  text-decoration: none;
+  color: rgba(255, 255, 255, 0.6);
 }
 
-
-nav a:hover{
-   font-size: larger;
-   color: rgba(255, 255, 255, 0.9);
-   transition: 0.5s;
+nav a:hover {
+  font-size: larger;
+  color: rgba(255, 255, 255, 0.9);
+  transition: 0.5s;
 }
-
 
 nav a.router-link-exact-active {
- /* color: #8B5160; */
- color: #008EA7;
- 
+  color: #008ea7;
 }
 
 @media only screen and (max-width: 600px) {
- 
- nav{
-  padding: 0px;
-  text-align: center;
-  margin: auto;
- }
- nav a{
-  font-size: small;
-  display: inline-block;
-  margin-right: 0.5em;
-  margin-left: 0.5em;
- }
+  nav {
+    padding: 0px;
+    text-align: center;
+    margin: auto;
+  }
+  nav a {
+    font-size: small;
+    display: inline-block;
+    margin-right: 0.5em;
+    margin-left: 0.5em;
+  }
 
- nav a:hover{
-  font-size: small;
-  transition: 0s;
- }
- 
-nav a.router-link-exact-active {
- font-size: small;
-} }
+  nav a:hover {
+    font-size: small;
+    transition: 0s;
+  }
+
+  nav a.router-link-exact-active {
+    font-size: small;
+  }
+}
 
 @media only screen and (max-width: 380px) {
   nav a {
     font-size: x-small;
   }
 
-nav a.router-link-exact-active {
- font-size: x-small; }
+  nav a.router-link-exact-active {
+    font-size: x-small;
+  }
 }
-
-  
 </style>
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,31 +1,45 @@
 <template>
-<div class="project-container">
-<ProjectCard projectname="Python Playground" projectdescription="En nettside hvor man kan sende inn Pythonscripts som blir utført av en backend" websitelink="/#" githublink="https://github.com/haakonka"/>
-<ProjectCard projectname="Egen nettside" projectdescription="Nettsiden du ser på akkurat nå" websitelink="/#" githublink="https://github.com/haakonka/portfolio"/>
-<ProjectCard projectname="Prosjekt Navn" projectdescription="Prosjektbeskrivelse" websitelink="/#" githublink="https://github.com/haakonka" />
-</div>
+  <div class="project-container">
+    <ProjectCard
+      projectname="Python Playground"
+      projectdescription="En nettside hvor man kan sende inn Pythonscripts som blir utført av en backend"
+      websitelink="/#"
+      githublink="https://github.com/haakonka/pythonplayground"
+    />
+    <ProjectCard
+      projectname="Egen nettside"
+      projectdescription="Nettsiden du ser på akkurat nå"
+      websitelink="/#"
+      githublink="https://github.com/haakonka/portfolio"
+    />
+    <ProjectCard
+      projectname="Prosjekt Navn"
+      projectdescription="Prosjektbeskrivelse"
+      websitelink="/#"
+      githublink="https://github.com/haakonka"
+    />
+  </div>
 </template>
 
 <script>
-import ProjectCard from '@/components/ProjectComponents/ProjectCard.vue'
+import ProjectCard from "@/components/ProjectComponents/ProjectCard.vue";
 export default {
-name: 'ProjectsView',
-components: {
-    ProjectCard
-}
-}
+  name: "ProjectsView",
+  components: {
+    ProjectCard,
+  },
+};
 </script>
 
 <style scoped>
-
-  .project-container{
-    display: flex;
-    flex-flow: row wrap;
-    justify-content: center;
-  }
-  @media only screen and (max-width: 660px) {
-  .project-container{
+.project-container {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+}
+@media only screen and (max-width: 660px) {
+  .project-container {
     display: block;
-  } }
-
+  }
+}
 </style>
