0bcec12575cae937ddb2394955f86d57ac82a4dd
haakonka <haakon.kanter@gmail.com>
2022-09-26 14:18:13 +0200

    Add municipalityproject

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -13,10 +13,10 @@
       githublink="https://github.com/haakonka/portfolio"
     />
     <ProjectCard
-      projectname="Prosjekt Navn"
-      projectdescription="Prosjektbeskrivelse"
-      websitelink="/#"
-      githublink="https://github.com/haakonka"
+      projectname="Kommune nettside"
+      projectdescription="Et raskt prosjekt, en nettside for å søke opp kommuner i Norge. Man kan også sortere på gyldige og utgåtte kommuner"
+      websitelink="https://kommune-3ebb7.web.app/"
+      githublink="https://github.com/haakonka/municipalityproject"
     />
   </div>
 </template>
